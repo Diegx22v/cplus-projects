4f0d54ed741e2bb4890f7682ac0e3d1e0e814612
Diegx22v <bydiego500@gmail.com>
2024-09-11 10:03:22 -0500

    ejercicio de array agregado :D

---
+++ array/array.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n[]={0,1,2,3,4,5};
+    cout<<"el tercer numero de la lista es: "<<n[2]<<endl;
+    return 0;
+}
