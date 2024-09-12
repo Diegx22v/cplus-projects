51b228022d26d2d33e8432e99fddbd763bd74345
Diegx22v <bydiego500@gmail.com>
2024-09-11 22:18:16 -0500

    agregamos una matriz

---
+++ matriz/matriz.cpp
@@ -0,0 +1,7 @@
+#include "iostream"
+using namespace std;
+int main(){
+    int matriz[2][4]={ {8,2,6,3} , {10,6,1,5} };
+    cout<<"el elemento de la fila 1 y columna 3 es: "<<matriz[0][2]<<endl;
+    return 0;
+}
